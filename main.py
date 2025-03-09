7f9c0d884a1ce3b45fcea58308d8947700cf64cd
Abner <abner.a.abreu.tamayo@gmail.com>
2025-03-09 17:55:56 -0400

    feat(models): add plotter to visualization package
    add scatter and line plotters

--- main.py
+++ main.py
@@ -1,18 +1,7 @@
-import matplotlib.pyplot as plt
-import src.models.verhulst_model as verhulst
-# Parámetros
+import src.models.verhulst_model as ver
+import src.visualization.plotter as plot
 
-iteraciones = 40 # Reducido para mejor visualización
+x = ver.logistic_model(0.3,1000,100,50)
 
-# Generar datos
-x = verhulst.logistic_model(0.3,1000,100,50)
-
-plt.figure(figsize=(10, 6))
-plt.plot(x, 'bo', markersize=8, linestyle='none')
-
-plt.xlabel('Iteración (n)', fontsize=12)
-plt.ylabel('Población', fontsize=12)
-plt.title('Modelo de Verhulst', fontsize=14)
-plt.grid(True, alpha=0.3)
-plt.legend()
-plt.show()
+fig = plot.line_plot(x)
+fig.show()
--- src/visualization/plotter.py
+++ src/visualization/plotter.py
@@ -1,1 +1,9 @@
-#Verhulst Model plotter
+import plotly.express as px
+import plotly.graph_objects as go
+def scatter_plot(data):
+    fig = px.scatter(data, 'Tiempo', 'Población', animation_frame=data)
+    return fig
+
+def line_plot(data):
+    fig = go.Figure(data = go.Scatter(y = data,mode ='lines', name= 'Función'))
+    return fig
